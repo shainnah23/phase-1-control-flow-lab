defba5e3e0928f73642f24a5abe89118f6e4b73b
joan@joan <mugurejoan3@gmail.com>
2025-06-24 15:29:40 +0300

    Updating index.js

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+// scuberGreetingForFeet function (already passing, but included for completeness)
+const scuberGreetingForFeet = (distance) => {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  }
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+// ternaryCheckCity function
+const ternaryCheckCity = (city) => {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+// switchOnCharmFromTip function
+const switchOnCharmFromTip = (tip) => {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+};
+
+// Export the functions so they can be accessed by the test file
+module.exports = {
+  scuberGreetingForFeet,
+  ternaryCheckCity,
+  switchOnCharmFromTip,
+};
